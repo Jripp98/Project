d0ca48e6c8ea7f0087b68f40945b69a99db2fc9a
Jripp low mechanic <92984375+Jripp98@users.noreply.github.com>
2022-05-15 00:10:34 +0700

    Update index.blade.php

--- Area Parkir/resources/views/index.blade.php
+++ Area Parkir/resources/views/index.blade.php
@@ -2,7 +2,6 @@
 @section('title', 'Utama')
 
 @section('judul', 'Halaman Utama')
-<img src="https://images5.alphacoders.com/102/1022371.jpg">
 @section('konten')
     <p>Website ini digunakan untuk mendata pemarkiran</p>
     <h2>Silahkan menambahkan Data Pengunjung</h2>
