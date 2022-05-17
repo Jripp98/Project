01ae6fd87da0b550e603ad26403bf0e097e3cc8e
Jripp low mechanic <92984375+Jripp98@users.noreply.github.com>
2022-05-18 05:55:06 +0700

    Update template.blade.php
    update manual

--- Area Parkir/resources/views/template.blade.php
+++ Area Parkir/resources/views/template.blade.php
@@ -38,7 +38,22 @@
     <h3>@yield('judul')</h3>
 
         @yield('konten')
-
+<footer class="footer">
+            
+        <p style="color:black; font-size:14px; font-weight:bold">create by: Arif Maulana
+        <br>Npm:2008107010026    
+        </p>            
+        <p style="color:white; font-size:14px; font-weight:bold">&copy; 2022 AreaParkir</p>
+        </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <style>
+        .footer{
+            position: absolute;
+            bottom: 0; 
+        }
+        .footer p{
+            font-style:bold;
+        }
+    </style>
 </body>
 </html>
